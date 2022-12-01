7f4b65ef03f9b944fc079c34a1e1b219d690047a
Zviad Mikeladze <zviadipf@gmail.com>
2022-12-01 17:50:58 +0400

    Adding item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecepieListComponent } from './recipes/recepie-list/recepie-list.component';
+import { RecepieDetailComponent } from './recipes/recepie-detail/recepie-detail.component';
+import { RecepieItemComponent } from './recipes/recepie-list/recepie-item/recepie-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, RecipesComponent, RecepieListComponent],
+  declarations: [AppComponent, HeaderComponent, RecipesComponent, RecepieListComponent, RecepieDetailComponent, RecepieItemComponent, ShoppingListComponent, ShoppingEditComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/recipes/recepie-list/recepie-list.component.ts
+++ src/app/recipes/recepie-list/recepie-list.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recepie-list',
   templateUrl: './recepie-list.component.html',
-  styleUrls: ['./recepie-list.component.css']
+  styleUrls: ['./recepie-list.component.css'],
 })
 export class RecepieListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test',
+      'test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg'
+    ),
+  ];
+  constructor() {}
 }
