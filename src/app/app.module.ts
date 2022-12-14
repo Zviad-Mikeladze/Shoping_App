bb6bb2e82a86a516cbc72f4525c32d96119a0f6a
Zviad-Mikeladze <zviadipf@gmail.com>
2022-12-14 15:56:11 +0400

    add new route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,9 +14,10 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing-module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, RecipesComponent, RecepieListComponent, RecepieDetailComponent, RecepieItemComponent, ShoppingListComponent, ShoppingEditComponent, DropdownDirective, RecipeStartComponent],
+  declarations: [AppComponent, HeaderComponent, RecipesComponent, RecepieListComponent, RecepieDetailComponent, RecepieItemComponent, ShoppingListComponent, ShoppingEditComponent, DropdownDirective, RecipeStartComponent, RecipeEditComponent],
   imports: [BrowserModule , FormsModule ,AppRoutingModule],
   providers: [ShoppingListService],
   bootstrap: [AppComponent],
